b2fbfe5525936f9167086c998dc97953962a68ce
Must <emailme>
2023-08-04 16:20:04 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // we should use our theme regulations inside the extend because of if we dont use like that we can lost default theme regulations
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class", // for darkmode theme 
+};
